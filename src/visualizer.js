52e6e97a3f21e10e729b5e646e2df9bddb09933a
Matti Leppänen <matti@mattijle.com>
2014-03-27 12:40:21 +0200

    Create visualizer.js

---
+++ src/visualizer.js
@@ -0,0 +1,31 @@
+function Visualizer(){
+  var visualizer = {};
+  visualizer.container = null;
+  visualizer.audioSource = null;
+  visualizer.fgCanvas = null;
+  visualizer.fgCtx = null;
+  visualizer.step = 3;
+  visualizer.init = function(opts){
+    visualizer.container = window.document.getElementById(opts.container);
+    visualizer.audioSource = opts.audioSource;
+    visualizer.fgCanvas = window.document.createElement('canvas');
+    visualizer.fgCtx = visualizer.fgCanvas.getContext('2d');
+    visualizer.container.appendChild(visualizer.fgCanvas);
+    visualizer.fgCanvas.width = visualizer.audioSource.streamData.length*2;
+    visualizer.fgCanvas.height = '128';
+  }
+  visualizer.start = function(){
+    draw(); 
+  }
+  var draw = function(){
+    visualizer.fgCtx.fillStyle = '#FFFFFF';
+    visualizer.fgCtx.fillRect(0,0,256,256);
+    visualizer.fgCtx.fillStyle = '#000000';
+    var step = visualizer.step;
+    for(var i = 0;i<visualizer.audioSource.streamData.length;i++){
+      visualizer.fgCtx.fillRect(i*step,Math.floor(62 - 62*(visualizer.audioSource.streamData[i]/256)),step,2);
+    }
+    requestAnimationFrame(draw);
+  };
+  return visualizer;
+}
